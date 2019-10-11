89d62991a949c767d6d929ed5b4d8646e9a1548e
Enci21 <eni.hegedus.95@gmail.com>
2019-10-11 17:11:27 +0200

    Add new test method

--- tests/FileWordAnalyzerTest.java
+++ tests/FileWordAnalyzerTest.java
@@ -1,18 +1,26 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileWordAnalyzerTest {
+    private FilePartReader reader = new FilePartReader("doc/test.txt", 1, 4);
+    private FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
 
     @Test
     public void testGetWordsOrderedAlphabeticallyIsAlphabetically(){
-        FilePartReader reader = new FilePartReader("doc/test.txt", 1, 4);
-        FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
         String[] words = reader.readLines().replace("\n", " ").split(" ");
         analyzer.getWordsOrderedAlphabetically().equals(words);
     }
 
-
+    @Test
+    public void testGetWordsContainingSubstring(){
+        List<String> words = new ArrayList<>();
+        words.add("banán");
+        words.add("ananász");
+        words.equals(analyzer.getWordsContainingSubstring("ná"));
+    }
 }
